883de3e5b65dfc9a1e185de06f428133ec83c98e
Jaydeep Patel <mcpe071202@gmail.com>
2022-10-25 21:07:07 +0530

    Initial out subtree commit gh-pages

---
+++ out/_next/static/_lQxCvKmZm8dlHxJvQUYp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/93fd850710307e3c.css","static/chunks/pages/index-ea502229ae37a59e.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
