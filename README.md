b5546a09d80a34a3b58054c873f17e3a3caa97e5
Jaydeep Patel <mcpe071202@gmail.com>
2022-10-27 17:11:21 +0530

    Added p5js and the graphs 4-6

--- README.md
+++ README.md
@@ -14,6 +14,16 @@ npm run dev
 
 ## Tech stack used
 
-NextJS
+NextJS (Allows me to generate a static version of the website)
+
+p5.js (for graphs)
 
 CSS
+
+## Note
+
+The graph data is hardcoded for now, but it can be easily brought in via an API call.
+
+p5.js is not the ideal choice for making graphs and I would prefer to use any graphing library instead.
+
+The graphs I made may or may not function well at extreme resolutions.
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,179 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import introStyles from '../styles/Intro.module.css'
+
+import React, { useEffect, useRef, useState } from "react";
+import dynamic from 'next/dynamic'
+
+
+function P5Sketch({ ref1 }) {
+  
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(ref1.current.clientWidth*1.083334, ref1.current.clientHeight).parent(canvasParentRef);
+    p5.noLoop();
+	};
+
+	const draw = (p5) => {
+    p5.background(255)
+    p5.fill(0);
+    p5.noStroke();
+    p5.circle(p5.width / 2, p5.height / 2, p5.min(p5.width / 2, p5.height / 2));
+	};
+
+  if (typeof window !== 'undefined') {
+    const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
+      ssr: false,
+    })
+    return <Sketch setup={setup} draw={draw} />;
+  }
+  else {
+    return null;
+  }
+};
+
+function P5Sketch4({ ref1 }) {
+  let space,initial,wid,n=3,mx=150,padding;
+
+  let data = [[125,"#74e0b0"],[105,"#60a498"],[30,"#ffc301"]];
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(ref1.current.clientWidth*1.083334, ref1.current.clientHeight).parent(canvasParentRef);
+    initial = p5.width*0.2
+    space = (p5.width-initial)/(n);
+    wid = space*0.6
+    
+    padding = p5.width*0.05
+    p5.noLoop();
+	};
+
+	const draw = (p5) => {
+    p5.background(255)
+    let hei = (p5.height-2*padding)/n;
+    p5.strokeWeight(3)
+    p5.stroke("#eee")
+    for(let i=150;i>=0;i-=50){
+      let y = padding + hei*(3 - i/50);
+      p5.line(0,y,p5.width,y)
+    }
+    
+    for(let i=0;i<n;i++){
+      
+      p5.fill(data[i][1])
+      p5.noStroke()
+      let x,y,x1,y1;
+      
+      x = initial+ i*space
+      x1 = wid
+      y = p5.height - padding
+      y1 =  -1*(y-padding)*(data[i][0]/150)
+      
+      p5.rect(x,y,x1,y1,wid*0.2)  
+      
+      
+      y1 = -1*(y-padding)*((data[i][0]-10)/150)
+      p5.fill(data[i][1]+"55")
+      p5.rect(x,y,x1*1.25,y1,wid*0.3)  
+    }
+	};
+
+  if (typeof window !== 'undefined') {
+    const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
+      ssr: false,
+    })
+    return <Sketch setup={setup} draw={draw} />;
+  }
+  else {
+    return null;
+  }
+};
+
+function P5Sketch5({ ref1 }) {
+  let wid;
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(ref1.current.clientWidth*1.083334, ref1.current.clientHeight).parent(canvasParentRef);
+    wid = p5.width*0.5;
+    p5.noLoop();
+	};
+
+	const draw = (p5) => {
+    p5.background(255);
+    p5.translate(p5.width/2,p5.height/2);
+    p5.noStroke()
+    p5.fill("#74e0b0");
+    p5.circle(0,0,wid)
+    
+    p5.fill("#60a398");
+    p5.arc(0,0,wid,wid,-1.2*p5.HALF_PI,0.2*p5.HALF_PI)
+    
+    p5.fill(255);
+    p5.circle(0,0,wid*0.66)
+	};
+
+  if (typeof window !== 'undefined') {
+    const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
+      ssr: false,
+    })
+    return <Sketch setup={setup} draw={draw} />;
+  }
+  else {
+    return null;
+  }
+};
+
+function P5Sketch6({ ref1 }) {
+  let n = 3;
+  let wid;
+  let data;
+  const setup = (p5, canvasParentRef) => {
+    p5.createCanvas(ref1.current.clientWidth*1.083334, ref1.current.clientHeight).parent(canvasParentRef);
+    wid = p5.width/(n*2)*0.5
+    data = [[-p5.HALF_PI,-.5*p5.HALF_PI,"#fdc300"],[-p5.HALF_PI,1.5*p5.HALF_PI,"#60a498"],[-p5.HALF_PI,2*p5.HALF_PI,"#74e0b0"]];
+    p5.noLoop();
+
+  
+	};
+
+	const draw = (p5) => {
+    p5.background(255)
+    p5.noStroke()
+    p5.translate(p5.width/2,p5.height/2)
+    for(let i=n;i>0;i--){
+      p5.fill("#eeeeee")
+      p5.circle(0,0,2*i*wid)  
+      
+      p5.fill(data[i-1][2])
+      p5.arc(0,0,2*i*wid,2*i*wid,data[i-1][0],data[i-1][1])
+      
+      //balls
+      
+      let x = ((i)*wid -wid/4)*p5.cos(data[i-1][1]);
+      let y = ((i)*wid -wid/4)*p5.sin(data[i-1][1]);
+      
+      p5.circle(x, y,wid/2)
+      
+      x = ((i)*wid -wid/4)*p5.cos(data[i-1][0]);
+      y = ((i)*wid -wid/4)*p5.sin(data[i-1][0]);
+      
+      p5.circle(x, y,wid/2)
+      
+      
+      p5.fill(255)
+      p5.circle(0,0,(2*i -1)*wid)
+    }
+	};
+
+  if (typeof window !== 'undefined') {
+    const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
+      ssr: false,
+    })
+    return <Sketch setup={setup} draw={draw} />;
+  }
+  else {
+    return null;
+  }
+};
+
 
 function Intro() {
   return (
@@ -33,6 +205,14 @@ export default function Home() {
   for (let index = 0; index < 10; index++) {
     arr.push(index*100);
   }
+
+  let refArr = []
+
+  for (let index = 0; index < 6; index++) {
+    const r1 = useRef(index);
+    refArr.push(r1);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -73,23 +253,35 @@ export default function Home() {
         <div className={styles.mainContentContainer}>
           <Intro />
           <div className={styles.mainGrid}>
-            <div className={styles.col + " border"}>
-              <Image src="/1.jpg" alt="Data Graph" layout='fill'/>
+            <div className={styles.col + " border"} ref={refArr[0]}>
+              <P5Sketch ref1={refArr[0]} />
             </div>
-            <div className={styles.col + " border"}>
-              <Image src="/2.jpg" alt="Data Graph" layout='fill'/>
+            <div className={styles.col + " border"} ref={refArr[1]}>
+              <div className={styles.graphHeading}>
+                Ticket Status
+              </div>
+              <Image src="/2.jpg" layout="fill" alt="graph" />
             </div>
-            <div className={styles.col + " border"}>
-              <Image src="/3.jpg" alt="Data Graph" layout='fill'/>
+            <div className={styles.col + " border"} ref={refArr[2]}>
+              <P5Sketch ref1={refArr[2]} />
             </div>
-            <div className={styles.col + " border"}>
-              <Image src="/4.jpg" alt="Data Graph" layout='fill'/>
+            <div className={styles.col + " border"} ref={refArr[3]}>
+              <div className={styles.graphHeading}>
+                Total Messages
+              </div>
+              <P5Sketch4 ref1={refArr[3]} />
             </div>
-            <div className={styles.col + " border"}>
-              <Image src="/5.jpg" alt="Data Graph" layout='fill'/>
+            <div className={styles.col + " border"} ref={refArr[4]}>
+              <div className={styles.graphHeading}>
+                Message Volume
+              </div>
+              <P5Sketch5 ref1={refArr[4]} />
             </div>
-            <div className={styles.col + " border"}>
-              <Image src="/6.jpg" alt="Data Graph" layout='fill'/>
+            <div className={styles.col + " border"} ref={refArr[5]}>
+              <div className={styles.graphHeading}>
+                Rates
+              </div>
+              <P5Sketch6 ref1={refArr[5]} />
             </div>
           </div>
         </div>
