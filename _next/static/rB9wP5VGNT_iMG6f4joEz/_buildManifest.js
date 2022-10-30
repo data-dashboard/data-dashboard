cc614251f2c1967eaa765400a46cc7b6cb4a3d27
Jaydeep Patel <mcpe071202@gmail.com>
2022-10-30 16:08:18 +0530

    Deploy gh-pages

--- _next/static/rB9wP5VGNT_iMG6f4joEz/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/921-ab4fbbf408885b5e.js","static/css/76a04d7c639718c9.css","static/chunks/pages/index-38c1f6c3e8386998.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
